d3edbfb01698450cce60ac49175792d29976b867
ppjjhh0515 <54648718+ppjjhh0515@users.noreply.github.com>
2020-07-15 00:28:25 -0700

    Create Variables-and-Types.py

---
+++ 01. Introduction-to-Python/1. Python Basics/Variables-and-Types.py
@@ -0,0 +1,60 @@
+# Create a variable savings
+savings = 100
+
+# Print out savings
+print(savings)
+
+#---
+
+# Create a variable savings
+savings = 100
+
+# Create a variable growth_multiplier
+growth_multiplier = 1.1
+
+# Calculate result
+result = savings * growth_multiplier**7
+
+# Print out result
+print(result)
+
+#---
+
+# Create a variable desc
+desc = 'compound interest'
+
+# Create a variable profitable
+profitable = True
+
+#---
+
+savings = 100
+growth_multiplier = 1.1
+desc = "compound interest"
+
+# Assign product of growth_multiplier and savings to year1
+year1 = savings * growth_multiplier
+
+# Print the type of year1
+print(type(year1))
+
+# Assign sum of desc and desc to doubledesc
+doubledesc = desc + desc
+
+# Print out doubledesc
+print(doubledesc)
+
+#---
+
+# Definition of savings and result
+savings = 100
+result = 100 * 1.10 ** 7
+
+# Fix the printout
+print("I started with $" + str(savings) + " and now have $" + str(result) + ". Awesome!")
+
+# Definition of pi_string
+pi_string = "3.1415926"
+
+# Convert pi_string into float: pi_float
+pi_float = float(pi_string)
