20e642c1668025b725d58b350633bfb9b29044a9
ppjjhh0515 <54648718+ppjjhh0515@users.noreply.github.com>
2020-07-15 02:47:13 -0700

    Create 3. Packages.py

---
+++ 01. Introduction-to-Python/3. Functions-and-Packages/3. Packages.py
@@ -0,0 +1,41 @@
+# Definition of radius
+r = 0.43
+
+# Import the math package
+import math
+
+# Calculate C
+C = 2 * math.pi * r
+
+# Calculate A
+A = math.pi * r ** 2
+
+# Build printout
+print("Circumference: " + str(C))
+print("Area: " + str(A))
+
+
+'''
+Perform a selective import from the math package where you only import the radians function
+
+Calculate the distance travelled by the Moon over 12 degrees of its orbit. Assign the result to dist. 
+You can calculate this as r * phi, where r is the radius and phi is the angle in radians. 
+To convert an angle in degrees to an angle in radians, use the radians() function, which you just imported.
+
+Print out dist.
+'''
+# Definition of radius
+r = 192500
+
+# Import radians function of math package
+from math import radians
+
+# Travel distance of Moon over 12 degrees. Store in dist.
+phi = radians(12)
+dist = r * phi
+
+# Print out dist
+print(dist)
+
+
+
