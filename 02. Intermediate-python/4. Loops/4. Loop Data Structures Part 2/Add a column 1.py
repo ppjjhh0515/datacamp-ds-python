bd9202526620396b99595a929eae84f841c44804
ppjjhh0515 <54648718+ppjjhh0515@users.noreply.github.com>
2020-09-20 23:10:41 -0400

    Update Add a column 1.py

--- 02. Intermediate-python/4. Loops/4. Loop Data Structures Part 2/Add a column 1.py
+++ 02. Intermediate-python/4. Loops/4. Loop Data Structures Part 2/Add a column 1.py
@@ -12,3 +12,14 @@ Use a for loop to add a new column, named COUNTRY, that contains a uppercase ver
 To see if your code worked, print out cars. Don't indent this code, so that it's not part of the for loop.
 '''
 
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Code for loop that adds COUNTRY column
+for lab, row in cars.iterrows() :
+    cars.loc[lab, 'COUNTRY'] = row['country'].upper()
+
+
+# Print cars
+print(cars)
