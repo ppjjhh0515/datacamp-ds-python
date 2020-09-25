f84437e4246577634d29e156f740061a359167bf
ppjjhh0515 <54648718+ppjjhh0515@users.noreply.github.com>
2020-09-24 20:37:16 -0400

    Create Index values and names.py

---
+++ Manipulating Dataframe with Pandas/2. Advanced Indexing./1. Index objects and labeled data/Index values and names.py
@@ -0,0 +1,16 @@
+'''
+Index values and names
+Which one of the following index operations does not raise an error?
+
+The sales DataFrame which you have seen in the videos of the previous chapter has been pre-loaded for you and is available for exploration in the IPython Shell.
+
+       eggs  salt  spam
+month                  
+Jan      47  12.0    17
+Feb     110  50.0    31
+Mar     221  89.0    72
+Apr      77  87.0    20
+May     132   NaN    52
+Jun     205  60.0    55
+'''
+sales.index = range(len(sales))
