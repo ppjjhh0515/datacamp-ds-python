b1528b031889ccd56465fd7630f2575474904c57
ppjjhh0515 <54648718+ppjjhh0515@users.noreply.github.com>
2020-10-03 16:17:21 -0400

    Create Tukey's comments on EDA.py

---
+++ Statistical Thinking in Python/1. Introduction to exploratory data analysis/Tukey's comments on EDA.py
@@ -0,0 +1,6 @@
+'''
+Tukey's comments on EDA
+Even though you probably have not read Tukey's book, I suspect you already have a good idea about his viewpoint from the video introducing you to exploratory data analysis. Which of the following quotes is not directly from Tukey?
+'''
+
+Often times EDA is too time consuming, so it is better to jump right in and do your hypothesis tests.
