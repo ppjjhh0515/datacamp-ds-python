cd71490cac3caff6461a1782483fa1b88a1b921c
ppjjhh0515 <54648718+ppjjhh0515@users.noreply.github.com>
2020-10-03 19:41:45 -0400

    Create Variance and covariance by looking.py

---
+++ Statistical Thinking in Python/2. Quantitative exploratory data analysis/4. Covariance and Pearson correlation coefficient/Variance and covariance by looking.py
@@ -0,0 +1,9 @@
+'''
+Variance and covariance by looking
+Consider four scatter plots of x-y data, appearing to the right. Which has, respectively,
+
+the highest variance in the variable x,
+the highest covariance,
+negative covariance?
+'''
+d,c,b
