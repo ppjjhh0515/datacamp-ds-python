a4e353856879f75f96779851a3ef743145f0e63e
ppjjhh0515 <54648718+ppjjhh0515@users.noreply.github.com>
2020-10-03 19:59:30 -0400

    Create Why do we use the language of probability?.py

---
+++ Statistical Thinking in Python/3. Thinking probabilistically-- Discrete variables/1. Probabilistic logic and statistical inference/Why do we use the language of probability?.py
@@ -0,0 +1,6 @@
+'''
+Why do we use the language of probability?
+Which of the following is not a reason why we use probabilistic language in statistical inference?
+'''
+
+Probabilistic language is not very precise.
