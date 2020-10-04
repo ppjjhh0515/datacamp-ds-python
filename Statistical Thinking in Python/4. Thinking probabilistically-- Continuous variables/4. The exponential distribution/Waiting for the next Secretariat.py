e22a19710e2f303b4a5639c66a8f6fb7c0aed79e
ppjjhh0515 <54648718+ppjjhh0515@users.noreply.github.com>
2020-10-03 21:29:24 -0400

    Create Waiting for the next Secretariat.py

---
+++ Statistical Thinking in Python/4. Thinking probabilistically-- Continuous variables/4. The exponential distribution/Waiting for the next Secretariat.py
@@ -0,0 +1,5 @@
+'''
+Waiting for the next Secretariat
+Unfortunately, Justin was not alive when Secretariat ran the Belmont in 1973. Do you think he will get to see a performance like that? To answer this, you are interested in how many years you would expect to wait until you see another performance like Secretariat's. How is the waiting time until the next performance as good or better than Secretariat's distributed? Choose the best answer.
+'''
+Exponential: A horse as fast as Secretariat is a rare event, which can be modeled as a Poisson process, and the waiting time between arrivals of a Poisson process is Exponentially distributed.
